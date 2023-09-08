a879774221c8fc20371898459865edfacd6b869f
shubham397 <shubhamkumar397@gmail.com>
2023-09-08 15:10:16 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export function App(props) {
+  const [pokemonName, setPokemonName] = useState("");
+  const [type, setType] = useState([]);
+  const [image, setImage] = useState("");
+  const [weakness, setWeakness] = useState([]);
+  const search = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonName.toLowerCase())
+      .then((result) => result.json())
+      .then((result) => {
+        setImage(result?.sprites?.other?.["official-artwork"]?.front_default);
+        let temp = [];
+        result?.types?.map((pokemon) => temp.push(pokemon?.type?.name));
+        setType(temp);
+
+        let weakTemp = new Set();
+        result?.types?.map((pokemon) => {
+          fetch(pokemon?.type?.url)
+            .then((r) => r.json())
+            .then((r) => {
+              r?.damage_relations?.double_damage_from?.map((type) =>
+                weakTemp.add(type.name)
+              );
+              setWeakness(Array.from(weakTemp));
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="Pokemon Name"
+        value={pokemonName}
+        onChange={(e) => {
+          setPokemonName(e.target.value);
+        }}
+      />
+      <br />
+      <button onClick={search}>Search</button>
+      {type.length > 0 && (
+        <>
+          <br />
+          <img alt="Pokemon" src={image} width="200" height="200" />
+
+          <div className="typeDiv">
+            <h3>Type</h3>
+            <div className="innerTypeDiv">
+              {type.map((t, index) => {
+                return <p className="type">{t}</p>;
+              })}
+            </div>
+          </div>
+
+          <div className="weaknessDiv">
+            <h3>Weakness</h3>
+            <div className="innerWeaknessDiv">
+              {weakness.map((t, index) => {
+                return <p className="weakness">{t}</p>;
+              })}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
